e82c296c7532ae9aa3f2762631b839651becbefc
87right <211973680+87right@users.noreply.github.com>
2025-10-11 20:19:08 +0900

    fix: 空中での移動がむしろ速くなる問題を解決 feat: 速さ制限を追加

--- src/main/java/com/right/github/world/entity/Player.java
+++ src/main/java/com/right/github/world/entity/Player.java
@@ -95,7 +95,7 @@ public class Player extends Entity {
                     if (onGround){
                         velocity.setX(5.0f);
                     }else {
-                        acceleration.addX(0.1f);
+                        acceleration.addX(0.01f);
                     }
                     if (getDisplayObjectData().getImage() == TEXTURE_R1){
                         getDisplayObjectData().setImage(TEXTURE_R2);
@@ -108,7 +108,7 @@ public class Player extends Entity {
                     if (onGround){
                         velocity.setX(-5.0f);
                     }else {
-                        acceleration.addX(-0.1f);
+                        acceleration.addX(-0.01f);
                     }
                     if (getDisplayObjectData().getImage() == TEXTURE_L1){
                         getDisplayObjectData().setImage(TEXTURE_L2);
@@ -128,6 +128,19 @@ public class Player extends Entity {
             }
         }
         pos = proPos;
+
+        // さすがに速さ無制限はまずい
+        if (velocity.getX() < -5.0f){
+            velocity.setX(-5.0f);
+        }else if (velocity.getX() > 5.0f){
+            velocity.setX(5.0f);
+        }
+
+        if (velocity.getY() < -5.0f){
+            velocity.setY(-5.0f);
+        }else if (velocity.getY() > 5.0f){
+            velocity.setY(5.0f);
+        }
 
         getDisplayObjectData().setX(pos.getX());
         getDisplayObjectData().setY(pos.getY());
