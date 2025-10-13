8ed768b6006bec6c9484b2a5a18ff409bce012df
87right <211973680+87right@users.noreply.github.com>
2025-10-13 18:40:14 +0900

    feat: Goalの追加

--- src/main/java/com/right/github/EditorMode.java
+++ src/main/java/com/right/github/EditorMode.java
@@ -1,13 +1,24 @@
 package main.java.com.right.github;
 
+import main.java.com.right.github.client.Client;
 import main.java.com.right.github.core.BlockPos;
 import main.java.com.right.github.shared.Logs;
+import main.java.com.right.github.shared.packet.Packet;
 import main.java.com.right.github.world.World;
 import main.java.com.right.github.world.level.block.state.BlockState;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 public class EditorMode {
     public static void main(String[] args){
         Logs.Info("これは本体ではなく、エディターモードです！");
+
+        Queue<Packet> packetsClientToServer = new ArrayDeque<>();
+        Queue<Packet> packetsServerToClient = new ArrayDeque<>();
+
+        Client client = new Client(packetsClientToServer, packetsServerToClient);
+
 
     }
 
---
+++ src/main/java/com/right/github/world/level/block/Goal.java
@@ -0,0 +1,7 @@
+package main.java.com.right.github.world.level.block;
+
+public class Goal extends Block{
+    public Goal(Properties pProperties) {
+        super(pProperties);
+    }
+}
